a25159f821d3b679abbe7517c7c318ded8240f6b
Amoriss <amoriss@protonmail.com>
2021-10-24 22:54:53 -0500

    changes made for params

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -11,22 +11,38 @@ namespace MethodExercise
         public static string shape;
         static void Main()
         {
-            Console.WriteLine("Welcome to MadLibs. Answer the following questions");
-            InputMethod();
-            Console.WriteLine($"Who lives in a {fruit} under the sea? Absorbant and {color} is he. If nautical " +
-                $"nonsense be something you wish? Then drop on the deck and flop like a {animal}. Ready?  Spongebob {shape} pants!");
+            //Console.WriteLine("Welcome to MadLibs. Answer the following questions");
+           // InputMethod();
+           // Console.WriteLine($"Who lives in a {fruit} under the sea? Absorbant and {color} is he. If nautical " +
+            //    $"nonsense be something you wish? Then drop on the deck and flop like a {animal}. Ready?  Spongebob {shape} pants!");
             int addAnswer = Add(5, 5);
             int paramAddAnswer = Add2(10, 10, 10);
+            int paramAddAnswer2 = Add2(10, 10, 10, 10); //answer is 40
             var multAnswer = Multiply(10, 10);
             var subAnswer = Subtract(20, 10);
+            Console.WriteLine("How many numbers would you like to add together?");
+            var numbers = int.Parse(Console.ReadLine());
+            int[] array = new int[numbers];
+            for (int i = 0; i < numbers; i++)
+            {
+                Console.WriteLine("Enter in a number:");
+                array[i] += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The total is:"+ Add2(array)); 
             var divAnswer = Divide(20, 5);
             var modAnswer = Mod(2, 2);
-            Console.WriteLine(addAnswer);
-            Console.WriteLine(paramAddAnswer);
-            Console.WriteLine(multAnswer);
-            Console.WriteLine(subAnswer);
-            Console.WriteLine(divAnswer);
-            Console.WriteLine(modAnswer);
+            var multAnswer2 = Multiply2(2, 2, 2); //answer is 8
+            var subAnswer2 = Subtract2(10, 5, 5); // answer is 0
+
+            Console.WriteLine($"5 + 5 = "+ addAnswer);
+            Console.WriteLine($"This is using params keyword for 3 numbers (Adding): {paramAddAnswer}");
+            Console.WriteLine($"This is using params keyword for 4 numbers (Adding): {paramAddAnswer2}");
+            Console.WriteLine($"10 x 10 = "+ multAnswer);
+            Console.WriteLine($"20 - 10 = " + subAnswer);
+            Console.WriteLine($"20 / 5 = " + divAnswer);
+            Console.WriteLine($"2/2 has a remainder of: "+ modAnswer);
+            Console.WriteLine($"This is using params keyword for 3 numbers (Multiplying) {multAnswer2}");
+            Console.WriteLine($"This is using params keyword for 3 numbers (Subtracting) {subAnswer2}");
         }
 
         public static void InputMethod()
@@ -76,6 +92,30 @@ namespace MethodExercise
             }
 
             return sum;
+        }
+
+        public static int Multiply2(params int[] nums)
+        {
+            int answer = 1;
+            foreach (var x in nums)
+            {
+                answer *= x;
+            }
+            return answer;
+            
+          
+        }
+
+        public static int Subtract2(params int[] nums)
+        {
+         
+          int answer = 0;
+          for (int i = nums[0]; i < nums.Length - 1; i++)
+          {
+              answer-=nums[i];
+          }
+          return answer; 
+          
         }
 
 
